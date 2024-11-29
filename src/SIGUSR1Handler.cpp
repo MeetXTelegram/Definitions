f0ca7e8a90382e9b7aabe10789305d4011ada433
username000101 <cxx.username000101@gmail.com>
2024-11-29 13:01:22 +0300

    Initial implementation of the SIGUSR1Handler

--- src/SIGUSR1Handler.cpp
+++ src/SIGUSR1Handler.cpp
@@ -1,7 +1,30 @@
 #include <Definitions.hpp>
 
+#include <iostream>
+
 void signalsUtils::SIGUSR1Handler(int signal) {
     if (signal != SIGUSR1) {
         spdlog::get("SignalHandler")->log(spdlog::level::warn, "Invalid signal provided({}) but expected {}", signal, SIGUSR1);
+        return;
+    }
+    spdlog::get("SignalHandler")->log(spdlog::level::warn, "The operation of MeetX was interrupted by signal {}, what should i do?\n\n1 -- Normal shutdown of MeetX\n2 -- Try to continue working");
+    while (true) {
+        int action; std::cin >> action;
+        switch (action) {
+            case 1: {
+                spdlog::get("SignalHandler")->log(spdlog::level::info, "Selected action: 1(shutdown)");
+                std::exit(signal);
+            }
+
+            case 2: {
+                spdlog::get("SignalHandler")->log(spdlog::level::info, "Selected action: 2(continue)");
+                return;
+            }
+
+            default: {
+                spdlog::get("SignalHandler")->log(spdlog::level::warn, "Invalid action provided, please try again");
+                continue;
+            }
+        }
     }
 }
